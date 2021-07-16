c274ead4e140423c7f82418c568e0e1910d6b13d
Terry Barthelemy <terrybarthelemy@icloud.com>
2021-07-16 14:20:57 -0400

    iOS projects ready to for github

---
+++ BullsEyeProject/BullsEye/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Barthelemy, Terry on 2/10/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
